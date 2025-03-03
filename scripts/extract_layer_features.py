135fac8af866443eca5b9239c8b35a165cf647d1
gatheluck <gatheluck@gmail.com>
2025-03-04 04:24:11 +0900

    feat: add

---
+++ scripts/extract_layer_features.py
@@ -0,0 +1,82 @@
+import argparse
+import pathlib
+
+import numpy as np
+import timm
+import torch
+import torchvision.datasets as datasets
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+from torch.utils.data import DataLoader
+
+from src.data import create_balanced_subset
+from src.v_information import extract_leaf_module_keys, extract_target_layer_features
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--num-sample-per-class",
+    "-n",
+    type=int,
+    default=20,
+)
+parser.add_argument(
+    "--batch-size",
+    "-b",
+    type=int,
+    default=32,
+)
+parser.add_argument(
+    "--save-path",
+    "-s",
+    type=pathlib.Path,
+    default=pathlib.Path("outputs/extracted_layer_features.npz"),
+)
+args = parser.parse_args()
+
+# TODO: load pre-trained model weights.
+
+# create a ResNet50 model.
+model = timm.create_model("resnet50", pretrained=True)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# create subset of ImageNet validation dataset.
+config = resolve_data_config({}, model=model)
+transform = create_transform(**config)
+dataset = datasets.ImageFolder(
+    root=pathlib.Path("data/imagenet/val"), transform=transform
+)
+subset_dataset = create_balanced_subset(dataset, args.num_sample_per_class)
+dataloader = DataLoader(
+    subset_dataset,
+    batch_size=args.batch_size,
+    shuffle=False,
+    num_workers=4,
+    pin_memory=True,
+)
+
+# extract specific target layers.
+# this specifies 1,10,20,30,40,49-th layers of the ResNet50 model.
+target_key_patterns = [
+    "^conv1$",
+    "^layer1.2.conv3$",
+    "^layer2.3.conv1$",
+    "^layer3.2.conv2$",
+    "^layer3.5.conv3$",
+    "^layer4.2.conv3$",
+]
+target_layer_keys = extract_leaf_module_keys(
+    model, target_key_patterns=target_key_patterns
+)
+
+# extract target layer features.
+features_dict = extract_target_layer_features(
+    model, dataloader, device, target_layer_keys
+)
+
+print(">> extracted layer features:")
+for k, v in features_dict.items():
+    print(f"{k}: shape {v.shape}")
+
+# save extracted features.
+np.savez(args.save_path, **features_dict)
+print(f">> extracted layer features are saved to `{args.save_path}`.")
