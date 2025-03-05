89f00c8e752cbbc9932b72c7f102ce396eeb1342
gatheluck <gatheluck@gmail.com>
2025-03-05 21:39:01 +0900

    feat: update train_dictionary.py

--- scripts/train_dictionary.py
+++ scripts/train_dictionary.py
@@ -20,10 +20,10 @@ parser.add_argument(
     help="Batch size for training the dictionary. If -1, use NMF instead of MiniBatchNMF.",
 )
 parser.add_argument(
-    "--penultimate-features-path",
+    "--penultimate-features-dir-path",
     "-i",
     type=pathlib.Path,
-    default=pathlib.Path("outputs/extracted_penultimate_features.npz"),
+    default=pathlib.Path("outputs/penultimate_features"),
 )
 parser.add_argument(
     "--save-path",
@@ -34,14 +34,17 @@ parser.add_argument(
 args = parser.parse_args()
 
 # load penultimate features.
-features_dict = np.load(args.penultimate_features_path)
+target_paths = list(
+    args.penultimate_features_dir_path.glob("extracted_penultimate_features_*.npz")
+)
+
+loaded_features = [np.load(f)["layer4.2.act3"] for f in target_paths]
+penultimate_features = np.concatenate(loaded_features, axis=0)
 
 print(">> loaded penultimate features:")
-for k, v in features_dict.items():
-    print(f"{k}: shape {v.shape}")
+print(f"shape {penultimate_features.shape}")
 
 # reshape penultimate features.
-penultimate_features = features_dict["layer4.2.act3"]
 B, C, H, W = penultimate_features.shape
 reshaped_penultimate_features = penultimate_features.transpose(0, 2, 3, 1).reshape(
     B * H * W, C
